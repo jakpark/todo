80392dc800cba67817e80c22ad3ded31bebbe968
jake park <jake@parkjake.com>
2018-07-23 22:07:02 -0400

    created express app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// express app
+const app = express();
+
+app.user(logger('dev'));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+// catch-all route, welcome in JSON
+app.get('*', (req, res) => res.status(200).send({
+  message: "Welcome to Express server.",
+}));
+
+module.exports = app;

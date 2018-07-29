778d30b0ee909432a71dcd4f7a8a32f66ec4f1c3
jake park <jake@parkjake.com>
2018-07-28 21:02:23 -0400

    add dotenv, add pg password

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+require('dotenv').config()
 // express app
 const app = express();
 

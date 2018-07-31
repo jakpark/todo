4a28d87357fa34908dc65c422b3b237f3cc02184
jake park <jake@parkjake.com>
2018-07-31 19:14:27 -0400

    refactor out of controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,8 @@
 const todosController = require('../controllers').todos;
 
 module.exports = (app) => {
-  app.get('/', (req, res) => res.status(200).send({
-    message: 'Welcome to the Todos API on root url /',
-  }));
+  app.get('/', (req, res) => res.status(200).send([]));
 
-  app.post('/', todosController.create);
+  app.post('/', (req, res) => res.send({"title": "a todo"}));
   app.delete('/', (req, res) => res.send({}));
 };

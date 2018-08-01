bf7b32105ddee71b20014a65fe0fe514ae2197c4
jake park <jake@parkjake.com>
2018-07-31 21:21:50 -0400

    wip

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,20 @@
 const todosController = require('../controllers').todos;
 
+let store = {}
+let id = 1;
 module.exports = (app) => {
-  app.get('/', (req, res) => res.status(200).send([]));
+  app.get('/', (req, res) => res.status(200).send([
+    store[req.id]
+  ]));
 
-  app.post('/', (req, res) => res.send({"title": "a todo"}));
-  app.delete('/', (req, res) => res.send({}));
+  app.post('/', (req, res) => {
+    console.log(`req.body: ${req.body.title}`)
+    store[id] = req.body
+    id += 1;
+    res.send({id: id, store[id]})
+  });
+  app.delete('/', (req, res) => {res.send({
+    
+    })
+  });
 };
